ade46fd0aa3b9ff82770497df1d85a9d7b8bddfa
Nick Efford <nick.efford@gmail.com>
2025-11-06 14:18:37 +0000

    Initial example of serving static content

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ktor Examples
+
+Small examples of how to do various things using [Ktor][ktor].
+
+`static`: example of serving static content from application classpath.
+
+
+[ktor]: https://ktor.io/
---
+++ static/src/Routing.kt
@@ -0,0 +1,9 @@
+import io.ktor.server.application.Application
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.routing.routing
+
+fun Application.configureRouting() {
+    routing {
+        staticResources("/", "static")
+    }
+}
