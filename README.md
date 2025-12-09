1de46db58ebacbe2208914b6dff7f71d542ac97e
Nick Efford <nick.efford@gmail.com>
2025-12-09 17:32:05 +0000

    Added SPA version of dice rolling application

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 Small examples of how to do various things using [Ktor][ktor].
 
-`html-dsl`: example of how a Ktor application can use a DSL to construct
-an HTML document dynamically and deliver it to a client.
-
 `static`: example of serving static content from the application classpath.
 
 `testing`: example of automated testing for a Ktor server.
+
+`html-dsl`: example of how a Ktor application can use a DSL to construct
+an HTML document dynamically and deliver it to a client.
+
+`dice-roller`: expansion of `html-dsl` with two separate pages, providing
+a fully interactive dice rolling application.
+
+`dice-spa`: alternative to `dice-roller`, operating as a single-page
+application, demonstrating the use of htmx and Pebble templates.
 
 
 [ktor]: https://ktor.io/
---
+++ dice-spa/src/Routing.kt
@@ -0,0 +1,37 @@
+import io.ktor.http.Parameters
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.pebble.respondTemplate
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+
+fun Application.configureRouting() {
+    routing {
+        get("/") { call.displayForm() }
+        post("/roll") { call.handleDiceRoll() }
+    }
+}
+
+private suspend fun ApplicationCall.displayForm() {
+    respondTemplate("form.html", model = mapOf(
+        "sidesOptions" to sidesOptions
+    ))
+}
+
+private suspend fun ApplicationCall.handleDiceRoll() {
+    val (dice, sides) = extractParameters(receiveParameters())
+    val results = diceRoll(dice, sides)
+    respondTemplate("results.html", model = mapOf(
+        "dice" to dice,
+        "sides" to sides,
+        "results" to results,
+        "total" to results.sum(),
+    ))
+}
+
+private fun extractParameters(params: Parameters) = Pair(
+    params["dice"]?.toInt() ?: error("Number of dice not specified"),
+    params["sides"]?.toInt() ?: error("Number of sides not specified")
+)
